e5deb833882f85c1fc28176fb5c206ebb97fd64c
Philip Peterson <philip.c.peterson@gmail.com>
2023-04-18 21:38:02 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import 'react'
 import face from "./assets/face.png"
+import sunglasses from "./assets/sunglasses.png"
 import './App.css'
 
+import * as S from './styles'
+import { useCallback, useEffect, useState } from 'react'
+
+
 function App() {
+  const [scrollY, setScrollY] = useState(0)
+  const [numLoaded, setNumLoaded] = useState(0)
+
+  useEffect(() => {
+    window.onscroll = () => {
+      setScrollY(window.scrollY)
+    }
+  }, [])
+
+  const onLoad = useCallback(() => {
+    setNumLoaded(oldNum => oldNum + 1)
+  }, [])
+
+  const opacity = 1 - Math.min(1, Math.max(0, (scrollY - 0) / 600))
+  const loading = numLoaded != 2
+
   return (
-    <div>
-      <img src={face} />
-    </div>
+    <S.Container>
+      <S.HeadFrame style={{ visibility: loading ? 'hidden' : 'visible'}}>
+        <S.Image src={face} onLoad={onLoad} />
+        <S.Image src={sunglasses}  onLoad={onLoad} style={{ position: 'absolute', top: 0, left: 0, opacity, }} />
+      </S.HeadFrame>
+    </S.Container>
   )
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: red;
+  text-align: center;
+  line-height: 0;
+  background: radial-gradient(circle at 50% 0%, rgba(27,31,51,1) 0%, rgba(76,54,47,1) 80%, rgba(74,175,184,1) 200%);
+`
+
+export const HeadFrame = styled.div`
+  position: relative;
+  display: inline-block;
+  border: 1px solid blue;
+  margin-top: 50vh;
+`
+
+export const Image = styled.img`
+  display: block;
+  width: 40vw;
+`
